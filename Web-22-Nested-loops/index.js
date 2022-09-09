e7045e5b3fa76ba9188eff091987209cc4a63be8
krishna8464 <yendavamshikrishna123@gmail.com>
2022-09-09 09:23:54 +0530

    Nested loop

---
+++ Web-22-Nested-loops/index.js
@@ -0,0 +1,26 @@
+
+// // for(let f = 1;f<=4;f++){
+// //  let bag = "";
+// //   let bag2 = "";
+// //   for(let s=1;s<=f;s++){
+// //     bag2 = bag2 + s
+// //     bag=bag + "*"
+// //   }
+// //   console.log(bag2)
+// //   console.log(bag)
+// // }
+// You are given an array of N integers. You need to printYesif 42 is present in array, else printNo
+var father=1;
+while(father<=16)
+{
+var son=1;
+var bag = "";
+while(son<=10)
+{
+bag=bag+"*";
+son++;
+}
+console.log(bag);
+father++;
+}
+
